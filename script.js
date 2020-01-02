85f9cb0397947807f5f8627fc0c342158e26da15
qualmless <terasmash@gmail.com>
2020-01-02 13:35:08 -0600

    everything works but the launchStatusCheck div

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+      let journeyArray = response.json().length;
+      const div = document.getElementById("missionTarget");
+         div.innerHTML = `
+            <h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[2].name}</li>
+                  <li>Diameter: ${json[2].diameter}</li>
+                  <li>Star: ${json[2].star}</li>
+                  <li>Distance from Earth: ${json[2].distance}</li>
+                  <li>Number of Moons: ${json[2].moons}</li>
+               </ol>
+               <img src="${json[2].image}">
+               `
+   });
+});
    let form = document.querySelector("form");
-   // console.log("*********");
-
-
    form.addEventListener("submit", function(event) {
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
@@ -15,7 +29,7 @@ window.addEventListener("load", function() {
          event.preventDefault();
       }
       
-      if ( (isNaN(fuelLevel)) || (isNaN(cargoMass)) ) {
+      if ( (isNaN(fuelLevel.value)) || (isNaN(cargoMass.value)) ) {
          alert("fuel and cargo amounts should be numbers!");
          event.preventDefault();
       }
@@ -30,38 +44,68 @@ window.addEventListener("load", function() {
          
          const div = document.getElementById("launchStatusCheck");
             div.innerHTML = `
-            <h2 id="launchStatus" style="color:red;">Shuttle Not Ready for Launch</h2>
+            <h2 id="launchStatus" style="color:red;">Shuttle Not Ready to Launch.</h2>
             <div  id="faultyItems" style="visibility: visible">
-                <ol>
+                <ol id="launchList">
                     <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
                     <li id="copilotStatus">Co-Pilot ${coPilotName.value} is ready for launch.</li>
                     <li id="fuelStatus">Fuel is too low for launch.</li>
                     <li id="cargoStatus">Cargo mass is too heavy for launch.</li>
                 </ol>
+            
                 `
                 event.preventDefault();
          } //End of 1st condition of launch
+      if((fuelLevel.value > 10000) && (cargoMass.value < 10000)){
+         alert("Shuttle Is Ready to Launch.");
+         // launchReady = true;
+               
+         const div = document.getElementById("launchStatusCheck");
+            div.innerHTML = `
+            <h2 id="launchStatus" style="color:green;">Shuttle ready to launch.</h2>
+            <div id="faultyItems" style="visibility: visible">
+               <ol id="launchList">
+                  <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                  <li id="copilotStatus">Co-Pilot ${coPilotName.value} is ready for launch.</li>
+                  <li id="fuelStatus">Fuel is sufficient for launch.</li>
+                  <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
+               </ol>
+            `
+            event.preventDefault();
+         }//End of 2nd condition
+
+         
+      if((fuelLevel.value > 10000) && (cargoMass.value > 10000)){
+         alert("Cargo too heavy to launch.");
+         const div = document.getElementById("launchStatusCheck");
+            div.innerHTML = `
+            <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
+            <div id="faultyItems" style="visibility: visible">
+               <ol id="launchList">
+                  <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                  <li id="copilotStatus">Co-Pilot ${coPilotName.value} is ready for launch.</li>
+                  <li id="fuelStatus">Fuel is sufficient for launch.</li>
+                  <li id="cargoStatus">Cargo mass too heavy for launch.</li>
+               </ol>
+            `
+            event.preventDefault();
+          }//End of 3rd condition
+
+
+      if((fuelLevel.value < 10000) && (cargoMass.value < 10000)){
+         alert("Shuttle not ready to launch.");
+            const div = document.getElementById("launchStatusCheck");
+               div.innerHTML = `
+                  <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
+                  <div id="faultyItems" style="visibility: visible">
+                     <ol id="launchList">
+                        <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                        <li id="copilotStatus">Co-Pilot ${coPilotName.value} is ready for launch.</li>
+                        <li id="fuelStatus">Fuel is too low for launch.</li>
+                        <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
+                     </ol>
+               `
+            event.preventDefault();
+         }//End of 4th condition
    });
-
-   // form.addEventListener("submit", function(event){
-   //    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-   //    if (fuelLevel < 10000) {
-   //       alert("fuel too low!");
-   //       event.preventDefault();
-   //    };
-   // });
 });
-
-
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
