35b14523c27988ed41e9f4e363a54882afeaa264
qualmless <terasmash@gmail.com>
2020-01-02 21:56:06 -0600

    updated script to display not ready to launch with first conditions (strings v no strings)

--- script.js
+++ script.js
@@ -32,81 +32,92 @@ window.addEventListener("load", function() {
       
       if ( (isNaN(fuelLevel.value)) || (isNaN(cargoMass.value)) ) {
          alert("fuel and cargo amounts should be numbers!");
+         const div = document.getElementById("launchStatusCheck");
+         div.innerHTML = `
+         <h2 id="launchStatus" style="color:red;">Shuttle not ready for launch.</h2>
+         `
          event.preventDefault();
       }
 
       if ( (!isNaN(pilotName.value)) || (!isNaN(copilotName.value)) ) {
          alert("Pilot Name and Co-Pilot Name should be strings!");
+         const div = document.getElementById("launchStatusCheck");
+         div.innerHTML = `
+         <h2 id="launchStatus" style="color:red;">Shuttle not ready for launch.</h2>
+         `
          event.preventDefault();
       }
+      
+      else {
 
-      if((fuelLevel.value < 10000) && (cargoMass.value > 10000)){
-         alert("Not enough fuel for journey.");
-         
-         const div = document.getElementById("launchStatusCheck");
-            div.innerHTML = `
-            <h2 id="launchStatus" style="color:red;">Shuttle Not Ready to Launch.</h2>
-            <div  id="faultyItems" style="visibility: visible">
-                <ol id="launchList">
-                    <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
-                    <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
-                    <li id="fuelStatus">Fuel is too low for launch.</li>
-                    <li id="cargoStatus">Cargo mass is too heavy for launch.</li>
-                </ol>
+         if((fuelLevel.value < 10000) && (cargoMass.value > 10000)){
+            alert("Not enough fuel for journey.");
             
-                `
-                event.preventDefault();
-         } //End of 1st condition of launch
-      else if((fuelLevel.value > 10000) && (cargoMass.value < 10000)){
-         alert("Shuttle Is Ready to Launch.");
-         // launchReady = true;
-               
-         const div = document.getElementById("launchStatusCheck");
-            div.innerHTML = `
-            <h2 id="launchStatus" style="color:green;">Shuttle ready to launch.</h2>
-            <div id="faultyItems" style="visibility: visible">
-               <ol id="launchList">
-                  <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
-                  <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
-                  <li id="fuelStatus">Fuel is sufficient for launch.</li>
-                  <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
-               </ol>
-            `
-            event.preventDefault();
-         }//End of 2nd condition
-
-         
-      else if((fuelLevel.value > 10000) && (cargoMass.value > 10000)){
-         alert("Cargo too heavy to launch.");
-         const div = document.getElementById("launchStatusCheck");
-            div.innerHTML = `
-            <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
-            <div id="faultyItems" style="visibility: visible">
-               <ol id="launchList">
-                  <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
-                  <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
-                  <li id="fuelStatus">Fuel is sufficient for launch.</li>
-                  <li id="cargoStatus">Cargo mass too heavy for launch.</li>
-               </ol>
-            `
-            event.preventDefault();
-          }//End of 3rd condition
-
-
-      else if((fuelLevel.value < 10000) && (cargoMass.value < 10000)){
-         alert("Shuttle not ready to launch.");
             const div = document.getElementById("launchStatusCheck");
                div.innerHTML = `
-                  <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
-                  <div id="faultyItems" style="visibility: visible">
-                     <ol id="launchList">
-                        <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
-                        <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
-                        <li id="fuelStatus">Fuel is too low for launch.</li>
-                        <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
-                     </ol>
+               <h2 id="launchStatus" style="color:red;">Shuttle Not Ready to Launch.</h2>
+               <div  id="faultyItems" style="visibility: visible">
+                  <ol id="launchList">
+                     <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                     <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
+                     <li id="fuelStatus">Fuel is too low for launch.</li>
+                     <li id="cargoStatus">Cargo mass is too heavy for launch.</li>
+                  </ol>
+               
+                  `
+                  event.preventDefault();
+            } //End of 1st condition of launch
+         else if((fuelLevel.value > 10000) && (cargoMass.value < 10000)){
+            alert("Shuttle Is Ready to Launch.");
+            // launchReady = true;
+                  
+            const div = document.getElementById("launchStatusCheck");
+               div.innerHTML = `
+               <h2 id="launchStatus" style="color:green;">Shuttle ready to launch.</h2>
+               <div id="faultyItems" style="visibility: visible">
+                  <ol id="launchList">
+                     <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                     <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
+                     <li id="fuelStatus">Fuel is sufficient for launch.</li>
+                     <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
+                  </ol>
                `
-            event.preventDefault();
-         }//End of 4th condition
+               event.preventDefault();
+            }//End of 2nd condition
+
+            
+         else if((fuelLevel.value > 10000) && (cargoMass.value > 10000)){
+            alert("Cargo too heavy to launch.");
+            const div = document.getElementById("launchStatusCheck");
+               div.innerHTML = `
+               <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
+               <div id="faultyItems" style="visibility: visible">
+                  <ol id="launchList">
+                     <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                     <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
+                     <li id="fuelStatus">Fuel is sufficient for launch.</li>
+                     <li id="cargoStatus">Cargo mass too heavy for launch.</li>
+                  </ol>
+               `
+               event.preventDefault();
+            }//End of 3rd condition
+
+
+         else if((fuelLevel.value < 10000) && (cargoMass.value < 10000)){
+            alert("Shuttle not ready to launch.");
+               const div = document.getElementById("launchStatusCheck");
+                  div.innerHTML = `
+                     <h2 id="launchStatus" style="color:red;">Shuttle not ready to launch.</h2>
+                     <div id="faultyItems" style="visibility: visible">
+                        <ol id="launchList">
+                           <li id="pilotStatus">Pilot ${pilotName.value} is ready for launch.</li>
+                           <li id="copilotStatus">Co-Pilot ${copilotName.value} is ready for launch.</li>
+                           <li id="fuelStatus">Fuel is too low for launch.</li>
+                           <li id="cargoStatus">Cargo mass is sufficient for launch.</li>
+                        </ol>
+                  `
+               event.preventDefault();
+            }//End of 4th condition
+      }
    });
 });
